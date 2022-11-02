9e6eb20f1b8b934bffff425cf8963e7a16629274
Code Cademy <dangkimberly26@gmail.com>
2022-11-01 19:46:35 -0700

    added boy clothing

--- client/src/components/tabs/clothingitems.js
+++ client/src/components/tabs/clothingitems.js
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react';
 
 const Items = () => {
   const [shirts, setShirts] = useState([]); 
+  const [boys, setBoys] = useState([]); 
 
   useEffect(() => {
     fetch("http://localhost:2626/womensclothing")
@@ -15,8 +16,17 @@ const Items = () => {
       });
   }, []);
 
+  useEffect(() => {
+    fetch("http://localhost:2626/mensclothing")
+      .then((response) => response.json())
+      .then((data) => {
+        setBoys(data);
+        
+      });
+  }, []);
 
-  if(!shirts){
+
+  if(!shirts || !boys){
         return <div>Loading...</div>
     } else { 
 
@@ -25,7 +35,6 @@ const Items = () => {
       <div className="card-deck">
         <h1> This is a Items component </h1>
         <div className="card">
-
               {shirts.map((shirt, index) => {
               return (
                 <div>
@@ -38,9 +47,24 @@ const Items = () => {
                 </div>
               );
               })}
-              
-          
         </div>
+
+        <div className="card">
+          {boys.map((boy, index) => {
+          return (
+            <div>
+              <img className="card-img-top" src={boy.image} alt="Card image cap" />
+              <div className="card-body">
+                <h5 className="card-title">{boy.title}</h5>
+                <p className="card-text">{boy.rating.rate}</p>
+                <p className="card-text">{boy.price}</p> 
+              </div>
+            </div>
+          );
+          })}
+
+
+         </div>
       </div>
 
     )
