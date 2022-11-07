8d032d7d13513cc3a947bf22a7c03c76a259d4a9
Code Cademy <dangkimberly26@gmail.com>
2022-11-07 14:56:14 -0800

    added a comment

--- client/src/components/tabs/cart.js
+++ client/src/components/tabs/cart.js
@@ -16,27 +16,29 @@ const Cart = () => {
     getCart();
   }, []);
 
-
+// if user not logged in then show them auth 0
     return (
-     
-      <div className="card-deck">
-         <div>
-            <h2>Cart Stuff</h2>
-            {/* <p>{cart.product_id}</p> */}
-        </div>
-        {cart.map((piece,index) => {
-            return (
-              <div className="card" style="width: 18rem;">
-                    {/* <img className="card-img-top" src={piece.image} alt="Card image cap" /> */}
-                  <div className="card-body">
-                        <h5 className="card-title">{piece.product_id}</h5>
-                          {/* <p className="card-text">Rating: {piece.rating.rate}</p>
-                          <p className="card-text">Price: {piece.price}</p>  */}
-                  </div>
-              </div>
-          );
-        })}
-    </div>
+     <div>
+      Hello KIMBERLY THIS IS THE CART
+     </div>
+    //   <div className="card-deck">
+    //      <div>
+    //         <h2>Cart Stuff</h2>
+    //         {/* <p>{cart.product_id}</p> */}
+    //     </div>
+    //     {cart.map((piece,index) => {
+    //         return (
+    //           <div className="card" style="width: 18rem;">
+    //                 {/* <img className="card-img-top" src={piece.image} alt="Card image cap" /> */}
+    //               <div className="card-body">
+    //                     <h5 className="card-title">{piece.product_id}</h5>
+    //                       {/* <p className="card-text">Rating: {piece.rating.rate}</p>
+    //                       <p className="card-text">Price: {piece.price}</p>  */}
+    //               </div>
+    //           </div>
+    //       );
+    //     })}
+    // </div>
       
     )
   }
