cbd1b2414277ef05294ee2d7452d58d6475f2ab7
Code Cademy <dangkimberly26@gmail.com>
2022-11-08 10:51:40 -0800

    fixing cart

--- client/src/components/tabs/cart.js
+++ client/src/components/tabs/cart.js
@@ -3,34 +3,26 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const Cart = () => {
   const [cart, setCart] = useState([]);
-  const [clothes, setClothes] = useState([]);
-  const {user} = useAuth0();
-
-
-  const addToCart = async (garment) => {
-    console.log('we are in user cart kamily')
-    console.log(user.sub)
-    setClothes([...clothes, garment])
-
-      const userCart = {
-        sub: user.sub,
-        id: garment.id,
-      }
-
-    }
+  const {user, isAuthenticated} = useAuth0();
+  
 
   //gets items from cart
   const getCart = () => {
-    fetch('/api/cart')
+    //gets dont have a body so we have to pass it as query parameters which is $=
+    fetch(`/api/cart?sub=${user.sub}`)
     .then((response) => response.json())
     .then((cart) => {
       setCart(cart);
     });
 }
   
+
   useEffect(() => {
-    getCart();
-  }, []);
+   if(isAuthenticated) {
+    getCart(cart);
+   }
+  }, [cart]);
+
 
 // if user not logged in then show them auth 0
     return (
@@ -44,7 +36,6 @@ const Cart = () => {
                       <div className="card-body">
                         <h5 className="card-title">{clothes.title}</h5>
                         <p className="card-text">Price: {clothes.price}</p> 
-                        <p className="card-text">ID {clothes.product_id}</p> 
                         {/* <button onClick={() => addToCart(shirt)}>Add to Cart</button> */}
                       </div>
                     </div>
--- server/routes/itemsordered.js
+++ server/routes/itemsordered.js
@@ -8,11 +8,17 @@ const router = express.Router();
 //GET REQUEST FOR ITEMS IN CART
 router.get("/", cors(),  async (req, res) => {
   const userCart = {
-    id: req.body.id,
+    sub: req.query.sub,
   }
+  console.log([userCart.sub])
     try {
-      const cart = await db.query('SELECT title, price, img, inventory.product_id, items_ordered.order_id, created_order.user_id , created_order.order_completed FROM inventory JOIN items_ordered ON inventory.product_id=items_ordered.product_id JOIN created_order ON items_ordered.order_id=created_order.order_id WHERE id=$1 AND created_order.order_completed IS true LIMIT 1; ', [userCart]);
-      res.send(cart);
+      const customerIdSql= 'SELECT id FROM users WHERE sub= $1';
+      const customerInfo = await db.query(customerIdSql, [userCart.sub])
+      //stores info of sub from front end to  customerId 
+      const customerId = customerInfo[0].id;
+
+      const shoppingCart = await db.query('SELECT title, price, img, inventory.product_id, items_ordered.order_id, created_order.user_id , created_order.order_completed FROM inventory JOIN items_ordered ON inventory.product_id=items_ordered.product_id JOIN created_order ON items_ordered.order_id=created_order.order_id WHERE created_order.user_id=$1 AND created_order.order_completed = FALSE ', [userCart.sub]);
+      res.send(shoppingCart);
     } catch (e) {
       return res.status(400).json({ e });
     }
