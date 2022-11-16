89c4634b2202780744b197d1ecac95c031b82d28
Code Cademy <dangkimberly26@gmail.com>
2022-11-16 08:29:09 -0800

    added alt

--- client/src/components/tabs/orderhistory.js
+++ client/src/components/tabs/orderhistory.js
@@ -47,7 +47,7 @@ useEffect(() => {
             return (
               <tr key = {index}>
                 <td>{his.created_on.slice(0,10)}</td>
-                <td> <img src={his.img} alt="" width="100px" /></td>
+                <td> <img src={his.img} alt="Shirt" width="100px" /></td>
                 <td>{his.title}</td>
                 <td>{his.order_id}</td>
               </tr>
