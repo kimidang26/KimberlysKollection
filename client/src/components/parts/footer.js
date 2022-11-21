e1d21868f7a8237f7f33107a1b82c69ae16429ac
Code Cademy <dangkimberly26@gmail.com>
2022-11-21 12:55:57 -0800

    fixed footer alignment added ig

--- client/src/components/parts/footer.js
+++ client/src/components/parts/footer.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import './footer.css';
+import {FaTwitterSquare} from "react-icons/fa";
+import {FaTiktok} from "react-icons/fa";
+import {FaInstagramSquare} from "react-icons/fa";
+
 
 function Footer() {
     return (
       <div className="footer">
         <div className="socialMedia">
             <div className="media-icons">
-            {/* <div className="twitter-icons"><FaTwitterSquare /></div>
-              <br></br>
-              <div className="tiktok-icons">< FaTiktok /></div> */}
-            </div>
+            <div className="twitter-icons"><FaTwitterSquare /></div>
+            <br></br>
+            <div className="tiktok-icons">< FaTiktok /></div>
+            <br></br>
+            <div className="ig-icons">< FaInstagramSquare /></div>
+          </div>
         </div>
-        
-        <p> &copy; 2022 KIMBERLYKOLLECTIONS.COM</p>
+        <div className='footer-icon'>
+          <p> &copy; 2022 KIMBERLYKOLLECTIONS.COM</p>
+        </div>
       </div>
     );
   }
