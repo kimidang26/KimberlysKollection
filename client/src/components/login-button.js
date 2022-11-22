6aa07c921eea0e42914b18914cd2c605c6ec210e
Code Cademy <dangkimberly26@gmail.com>
2022-11-21 23:53:16 -0800

    fixed home css

--- client/src/components/login-button.js
+++ client/src/components/login-button.js
@@ -2,12 +2,13 @@
 
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import "../components/nav-bar.css";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
   return (
     <button
-      className="btn btn-primary btn-block"
+      className="btn btn-default"
       onClick={() => loginWithRedirect()}
     >
       Log In
