5a43c9cab86bb54859097f72bece8a06e61a0447
Code Cademy <dangkimberly26@gmail.com>
2022-11-23 12:39:56 -0800

    changed to shop

--- client/src/components/nav-bar.js
+++ client/src/components/nav-bar.js
@@ -37,7 +37,7 @@ const NavBar = ({displayVideo}) => {
                 </li>
 
                 <li>
-                  <NavLink to="/clothing"> Clothes</NavLink>
+                  <NavLink to="/clothing"> Shop</NavLink>
                 </li>
                       
                   <li>
